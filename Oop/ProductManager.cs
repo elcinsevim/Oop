2aeb6ede4a26a1e9d8a18aae7c3541f6663902ab
Elçin SEVİM <46248880+elcinsevim@users.noreply.github.com>
2021-01-20 23:57:32 +0300

    Add project files.

---
+++ Oop/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager //ürünle ilgili operasyon ekleme,silme,listeme vb.
+    {
+        //encapsulation
+        public void Add( Product product)
+        {
+            Console.WriteLine(product.ProductName+"eklendi");
+        }
+        public void Updata(Product product)
+        {
+            Console.WriteLine(product.ProductName + "güncellendi");
+        }
+        //void -bir bilgiye ihtiyaç yoksa 
+
+
+        
+          
+            //void git yap bitir ama eğer sonucta cıkan değere ihtiyacın varsa int kullanırsın
+        
+    }
+}
---
+++ Oop/Program.cs
@@ -0,0 +1,33 @@
+﻿using OOP1;
+using System;
+
+namespace Oop
+{
+    class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+
+          
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+            product1.ProductName = "Masa";
+            //pascalcase //camelcase
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+            Console.WriteLine(product1.ProductName);
+
+           
+            //int,doble,bool...vb değer tip-değer üstünden olur ve bağlantı kopar
+            //diziler,class,abstract class,interface -ref no ile yapılır 
+
+        }
+    }
+}
